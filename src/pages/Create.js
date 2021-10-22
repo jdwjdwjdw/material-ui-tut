3280eab820b2e5c3637570646d896ba206e8c9d3
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-10-21 18:08:54 -0700

    Add custom theme

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-// import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
+
 
 export default function Create() {
   return (
@@ -20,16 +20,6 @@ export default function Create() {
       <Button onClick={() => console.log('you clicked me')} type="submit" color="secondary" variant="contained" endIcon={<KeyboardArrowRightIcon />}>
         Submit
       </Button>
-
-      {/* Icons */}
-      {/* <br />
-      <AcUnitOutlinedIcon />
-      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
-      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
-      <AcUnitOutlinedIcon color="action" fontSize="small" />
-      <AcUnitOutlinedIcon color="error" fontSize="small" />
-      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
-
 
     </Container>
   )
