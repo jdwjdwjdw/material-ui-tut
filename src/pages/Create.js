90761fed4d972f6d4877cf6c1c317eadd9793cda
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-10-25 20:38:41 -0700

    Add TextField to create page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { TextField } from '@mui/material';
 
 
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == '') {
+      setTitleError(true)
+    }
+
+    if (details == '') {
+      setDetailsError(true)
+    }
+
+    if (title && details) {
+      console.log(title, details)
+    }
+  }
+
   return (
     <Container>
       <Typography
@@ -17,9 +41,48 @@ export default function Create() {
         Create a New Note
       </Typography>
 
-      <Button onClick={() => console.log('you clicked me')} type="submit" color="secondary" variant="contained" endIcon={<KeyboardArrowRightIcon />}>
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+          sx={{
+            marginTop: 2,
+            marginBottom: 2,
+            display: 'block',
+          }}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={detailsError}
+          multiline
+          rows={4}
+          sx={{
+            marginTop: 2,
+            marginBottom: 2,
+            display: 'block',
+          }}
+        />
+
+        <Button
+          type="submit" 
+          color="secondary" 
+          variant="contained" 
+          endIcon={<KeyboardArrowRightIcon />}
+          >
+            Submit
+        </Button>
+      </form>
+
 
     </Container>
   )
