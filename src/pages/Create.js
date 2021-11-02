4bbdf2dc3164182873a41134d64c5465bf1b1da7
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-11-01 18:25:34 -0700

    Add notes via json-server

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -9,9 +9,11 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import { useHistory } from 'react-router';
 
 
 export default function Create() {
+  const history = useHistory()
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
   const [titleError, setTitleError] = useState(false)
@@ -32,7 +34,11 @@ export default function Create() {
     }
 
     if (title && details) {
-      console.log(title, details, category)
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: { "Content-type": "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/'))
     }
   }
 
